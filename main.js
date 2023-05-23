ee22e4c65f2f896fd479fb24f8b4b00c247b4b5d
animemuically6@gamil.com <animemuically6@gamil.com>
2023-05-23 13:39:18 +0530

    wed side

--- main.js
+++ main.js
@@ -4,8 +4,9 @@ var main = document.querySelector('#main');
    var imga= document.querySelector('#page>img');
 
 main.addEventListener("mousemove",function(dets){
-  imga.style.top = dets.y*0.05  + "px";
-    imga.style.left = dets.x*0.05 + "px";
+  imga.style.top = 1-dets.y*0.05  + "px";
+    imga.style.right= 1-dets.x*0.05 + "px";
         console.log(dets.x);
 
 })
+
